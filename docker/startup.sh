b8ea719cd48070dd3229d72132ae52463e523413
paul <bares.paul@gmail.com>
2024-02-02 11:56:12 +0400

    add ui-starter

--- docker/startup.sh
+++ docker/startup.sh
@@ -5,7 +5,15 @@ if [ -d "$SQUASHQL_TS_PATH" ]; then
     echo 'Directory exists'
 else
     cp -r squashql-ts $SQUASHQL_TS_PATH
-	npm i --prefix $SQUASHQL_TS_PATH @squashql/squashql-js
+	npm i --prefix $SQUASHQL_TS_PATH
+fi
+
+SQUASHQL_UI_PATH=/code/squashql-ui-starter
+if [ -d "$SQUASHQL_UI_PATH" ]; then
+    echo 'Directory exists'
+else
+    cp -r squashql-ui-starter $SQUASHQL_UI_PATH
+	npm i --prefix $SQUASHQL_UI_PATH
 fi
 
 # Start the first process
