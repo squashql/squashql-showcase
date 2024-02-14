2148d03dfa7f785361881dfb11ac8012dc6674d9
paul <bares.paul@gmail.com>
2024-02-14 16:03:52 +0400

    next conf

--- ui/next.config.mjs
+++ ui/next.config.mjs
@@ -1,6 +1,9 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  assetPrefix: isProd ? 'https://squashql.github.io/squashql-showcase' : undefined,
 }
 
 export default nextConfig
