95fb50e5a6648761f8cd76b3b766df4ebe731b38
paul <bares.paul@gmail.com>
2024-02-19 17:14:55 +0100

    migrate to 1.12.0

--- ui/src/app/spending/spending.ts
+++ ui/src/app/spending/spending.ts
@@ -1,25 +1,68 @@
 import {
-  GroupColumnSet,
-  comparisonMeasureWithinSameGroup, comparisonMeasureWithParent,
+  comparisonMeasureWithGrandTotal, comparisonMeasureWithGrandTotalAlongAncestors,
+  comparisonMeasureWithinSameGroup,
+  comparisonMeasureWithParent,
   comparisonMeasureWithPeriod,
   ComparisonMethod,
   Field,
-  from, integer,
-  Measure, multiply, PivotConfig,
+  from,
+  GroupColumnSet,
+  integer,
+  Measure,
+  multiply,
+  PivotConfig,
   Query,
   QueryMerge,
-  sum, TableField,
+  sum,
+  TableField,
   Year
 } from "@squashql/squashql-js"
 import {spending} from "@/app/lib/tables"
 import {QueryProvider} from "@/app/lib/queryProvider"
 
-class PercentOfParentMeasure implements Measure {
-  readonly alias: string = "% amount on rows"
-  readonly class: string = ""
+interface MeasureProvider {
+  create(ancestors: Field[]): Measure
+
+  axis: "row" | "column"
 }
 
-const pop = new PercentOfParentMeasure()
+type MeasureProviderType = Measure & MeasureProvider
+
+function isMeasureProviderType(m: Measure): m is MeasureProviderType {
+  return "create" in m && "axis" in m
+}
+
+class CompareWithGrandTotalAlongAncestors implements MeasureProviderType {
+  readonly class: string = ""
+
+  constructor(readonly alias: string, readonly underlying: Measure, readonly axis: "row" | "column") {
+  }
+
+  create(ancestors: Field[]): Measure {
+    const ratio = comparisonMeasureWithGrandTotalAlongAncestors("percent_of_" + this.axis, ComparisonMethod.DIVIDE, this.underlying, ancestors)
+    return multiply(this.alias, integer(100), ratio)
+  }
+}
+
+class PercentOfParentAlongAncestors implements MeasureProviderType {
+  readonly class: string = ""
+
+  constructor(readonly alias: string, readonly underlying: Measure, readonly axis: "row" | "column") {
+  }
+
+  create(ancestors: Field[]): Measure {
+    const ratio = comparisonMeasureWithParent("percent_of_parent_" + this.axis, ComparisonMethod.DIVIDE, this.underlying, ancestors)
+    return multiply(this.alias, integer(100), ratio)
+  }
+}
+
+const amount = sum("amount", spending.amount)
+const popOfRow = new CompareWithGrandTotalAlongAncestors("amount - % on rows", amount, "row")
+const popOfParentOnRows = new PercentOfParentAlongAncestors("amount - % parent on rows", amount, "row")
+const popOfCol = new CompareWithGrandTotalAlongAncestors("amount - % on columns", amount, "column")
+const popOfParentOnCols = new PercentOfParentAlongAncestors("amount - % parent on columns", amount, "column")
+const popOfGT = multiply("amount - % of Grand Total", comparisonMeasureWithGrandTotal("amount_percent_gt", ComparisonMethod.DIVIDE, amount), integer(100))
+
 const groupOfCountries = new TableField("group of countries")
 const countryGroups = new Map(Object.entries({
   "comp. with fr": ["france", "usa", "uk"],
@@ -28,7 +71,6 @@ const countryGroups = new Map(Object.entries({
 }))
 
 function createSpendingMeasures(): Measure[] {
-  const amount = sum("amount", spending.amount)
   const yoyGrowth = comparisonMeasureWithPeriod(
           "YoY Growth",
           ComparisonMethod.ABSOLUTE_DIFFERENCE,
@@ -39,14 +81,7 @@ function createSpendingMeasures(): Measure[] {
           ComparisonMethod.ABSOLUTE_DIFFERENCE,
           amount,
           new Map([[spending.country, "first"]]))
-  return [amount, yoyGrowth, amountComparison, pop]
-}
-
-function createPercentOfParentMeasure(pop: PercentOfParentMeasure, fields: Field[]): Measure {
-  const ancestors: Field[] = fields
-  const amount = sum("amount", spending.amount)
-  const ratio = comparisonMeasureWithParent(pop.alias + "_underlying", ComparisonMethod.DIVIDE, amount, ancestors)
-  return multiply(pop.alias, integer(100), ratio)
+  return [amount, yoyGrowth, amountComparison, popOfCol, popOfRow, popOfGT, popOfParentOnRows, popOfParentOnCols]
 }
 
 function createSpendingFields(): Field[] {
@@ -62,20 +97,21 @@ export class SpendingQueryProvider implements QueryProvider {
   readonly measures = spendingMeasures
 
   query(select: Field[], values: Measure[], pivotConfig: PivotConfig): QueryMerge | Query {
-    const index = values.indexOf(pop)
-    const copy = [...values]
-    if (index >= 0) {
-      const pop = copy[index]
-      copy.splice(index, 1)
-      copy.push(createPercentOfParentMeasure(pop, pivotConfig.rows))
-    }
+    const measures = values.map(m => {
+      if (isMeasureProviderType(m) && m.axis === "row") {
+        return m.create(pivotConfig.rows)
+      } else if (isMeasureProviderType(m) && m.axis === "column") {
+        return m.create(pivotConfig.columns)
+      }
+      return m
+    })
 
     const gocIndex = select.indexOf(groupOfCountries)
     select = select.filter(f => f !== groupOfCountries)
     const columnSets = gocIndex >= 0 ? [new GroupColumnSet(groupOfCountries, spending.country, countryGroups)] : []
 
     return from(spending._name)
-            .select(select, columnSets, copy)
+            .select(select, columnSets, measures)
             .build()
   }
 }
