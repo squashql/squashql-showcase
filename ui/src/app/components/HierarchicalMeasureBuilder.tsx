fde5e4202df39e2ef8c735114d95e6ee3dca0d0c
paul <bares.paul@gmail.com>
2024-02-29 18:56:17 +0400

    fix title

--- ui/src/app/components/HierarchicalMeasureBuilder.tsx
+++ ui/src/app/components/HierarchicalMeasureBuilder.tsx
@@ -98,7 +98,7 @@ export default function HierarchicalMeasureBuilder(props: HierarchicalMeasureBui
               <div className="modal-dialog">
                 <div className="modal-content">
                   <div className="modal-header">
-                    <h1 className="modal-title fs-5" id="hiercompModalLabel">Time period comparison</h1>
+                    <h1 className="modal-title fs-5" id="hiercompModalLabel">Hierarchical comparison</h1>
                     <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                   </div>
                   <div className="modal-body">
