bde4633548f5345e1d7ae723f8435d3d3715a276
paul <bares.paul@gmail.com>
2024-03-26 16:47:23 +0100

    fix default formatter

--- ui/src/app/lib/formatters.ts
+++ ui/src/app/lib/formatters.ts
@@ -3,7 +3,7 @@ export interface Formatter {
   formatter: (v: any) => string
 }
 
-const nf = new Intl.NumberFormat("en-US", {maximumFractionDigits: 2})
+const nf = new Intl.NumberFormat("en-US", {style: "decimal", maximumFractionDigits: 2})
 const eurf = new Intl.NumberFormat("en-US", {style: "currency", currency: "USD"})
 const usdf = new Intl.NumberFormat("en-US", {style: "currency", currency: "USD"})
 const cashf = Intl.NumberFormat("en-US", {
@@ -28,7 +28,7 @@ const cashFormatter: Formatter = {
 
 export const defaultNumberFormatter: Formatter = {
   label: "4,999.99",
-  formatter: (v: any) => typeof v === 'number' && v % 1 != 0 ? nf.format(v) : v
+  formatter: (v: any) => nf.format(v)
 }
 
 const none: Formatter = {
