3ec8878ba2770a3d90eec560496eb005ec517c16
paul <bares.paul@gmail.com>
2024-04-22 16:39:29 +0400

    persist mapping forecast

--- ui/src/app/financialplanning/page.tsx
+++ ui/src/app/financialplanning/page.tsx
@@ -4,7 +4,7 @@ import UploadFile from "@/app/components/UploadFile"
 import dynamic from "next/dynamic"
 import ForecastPeriodSelector from "@/app/components/ForecastPeriodSelector"
 import React, {useEffect, useState} from "react"
-import {clearCurrentState} from "@/app/lib/dashboard"
+import {clearCurrentState, deserialize_} from "@/app/lib/dashboard"
 import MappingConfigurator from "@/app/components/MappingConfigurator"
 import {queryExecutor} from "@/app/lib/queries"
 import {Field, TableField} from "@squashql/squashql-js"
@@ -31,17 +31,30 @@ interface ForecastPageState {
   version: number
 }
 
-const storageKey = "state#fpna"
+const dashboardStorageKey = "dashboard#state#fpna"
+const pageStorageKey = "page#state#fpna"
+const tableName = "forecast"
 const defaultYear = new Date().getFullYear() - 1
 
-export default function Page() {
-  const [state, setState] = useState<ForecastPageState>({
+export function computeInitialState(key: string): ForecastPageState {
+  if (typeof window !== "undefined") {
+    const data = window.localStorage.getItem(key)
+    if (data) {
+      return deserialize_(data)
+    }
+  }
+  return {
     year: defaultYear,
     version: 0
-  })
+  }
+}
+
+export default function Page() {
+  const [didMount, setDidMount] = useState(false)
+  const [state, setState] = useState<ForecastPageState>(computeInitialState(pageStorageKey))
 
   function onForecastPeriodSelected(year: number, month?: number) {
-    clearCurrentState(storageKey)
+    clearCurrentState(dashboardStorageKey)
     setState((prevState: ForecastPageState) => {
       return {
         ...prevState,
@@ -53,7 +66,7 @@ export default function Page() {
   }
 
   function onMappingDefined(mapping: ForecastFields) {
-    clearCurrentState(storageKey)
+    clearCurrentState(dashboardStorageKey)
     setState((prevState: ForecastPageState) => {
       return {
         ...prevState,
@@ -63,7 +76,7 @@ export default function Page() {
     })
   }
 
-  function fetch() {
+  function fetch(resetMapping: boolean) {
     queryExecutor.querier.getMetadata()
             .then(r => {
               const store = r.stores.find(s => s.name === tableName)
@@ -76,7 +89,7 @@ export default function Page() {
                       _name: store.name,
                       _fields: fields
                     },
-                    mapping: undefined, // reset the mapping
+                    mapping: resetMapping ? undefined : prevState.mapping,
                     version: prevState.version + 1
                   }
                 })
@@ -85,26 +98,34 @@ export default function Page() {
   }
 
   function onFileUploaded() {
-    clearCurrentState(storageKey)
-    fetch()
+    clearCurrentState(dashboardStorageKey)
+    fetch(true)
   }
 
   useEffect(() => {
-    fetch()
+    setDidMount(true)
+  }, [])
+
+  useEffect(() => {
+    window.localStorage.setItem(pageStorageKey, JSON.stringify(state))
+  }, [state])
+
+  useEffect(() => {
+    fetch(false)
   }, [state.table?._fields.map(e => getElementString(e)).join()])
   // Use join array as dependency because Object.is(["a"], ["a"]) is false
 
-  const tableName = "forecast"
   if (state.table) {
     const qp = new ForecastQueryProvider(state.table, state.mapping, state.year, state.month)
-    return (
+    return didMount && (
             <>
               <Dashboard title={"Financial planning and analysis"}
                          key={"d" + state.version} // use key to force to recreate the component. Needed to properly force the local storage to be updated.
                          queryProvider={qp}
-                         storageKey={storageKey}
+                         storageKey={dashboardStorageKey}
                          menuElements={[
-                           <SettingsMenu key={"settings"} year={state.year} month={state.month}/>,
+                           <SettingsMenu key={"settings"} year={state.year} month={state.month}
+                                         isMappingDefined={state.mapping !== undefined}/>,
                          ]}
                          elements={[<UploadFile key={"uf"} table={tableName} onFileUploaded={onFileUploaded}/>]}/>
               <ForecastPeriodSelector onForecastPeriodSelected={onForecastPeriodSelected}/>
@@ -118,17 +139,20 @@ export default function Page() {
 }
 
 interface SettingsMenuProps {
-  year: number
+  year?: number
   month?: number
+  isMappingDefined: boolean
 }
 
 function SettingsMenu(props: SettingsMenuProps) {
+  const c = getNumberOfIncorrectSettings(props)
   return (
           <div className="col px-0 mx-1 my-1">
             <div className="dropdown">
               <button className="btn btn-light btn-sm dropdown-toggle" type="button" data-bs-toggle="dropdown"
                       aria-expanded="false">
                 Settings
+                {c > 0 && <>&nbsp;<span className="badge text-bg-danger rounded-pill">{c}</span></>}
               </button>
               <ul className="dropdown-menu">
                 <li>
@@ -136,15 +160,35 @@ function SettingsMenu(props: SettingsMenuProps) {
                      href="#"
                      data-bs-toggle="modal"
                      data-bs-target="#forecastperiodselectorModal">
-                    Actual end period (year = {props.year}, month = {props.month})
+                    Actual end period (year={props.year}{props.month && `, month=${props.month}`})
                   </a>
                 </li>
                 <li>
                   <a className="dropdown-item" href="#" data-bs-toggle="modal"
-                     data-bs-target="#mappingconfiguratorModal">Configure column mapping</a>
+                     data-bs-target="#mappingconfiguratorModal">Configure column mapping
+                    {!props.isMappingDefined && <>&nbsp;<span className="badge text-bg-danger rounded-pill">1</span></>}
+                  </a>
+                </li>
+                <li>
+                  <hr className="dropdown-divider"/>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#" onClick={() => clearCurrentState(pageStorageKey)}>Clear
+                    settings</a>
                 </li>
               </ul>
             </div>
           </div>
   )
+}
+
+function getNumberOfIncorrectSettings(props: SettingsMenuProps) {
+  let count = 0
+  if (!props.isMappingDefined) {
+    count++
+  }
+  if (props.year === undefined) {
+    count++
+  }
+  return count
 }
